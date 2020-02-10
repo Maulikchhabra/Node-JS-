c0711cf0f3cb05a53415763cbf97517cc6acb298
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-02-10 21:33:02 +0530

    Chalk package from NPM

---
+++ Challenges/challenge3/app.js
@@ -0,0 +1,8 @@
+//Challenge 3
+  //Chalk package
+const chalk =require('chalk') //Importing chalk package
+console.log(chalk.green("Success!")); //using .green method to turn string in green
+console.log(chalk.inverse("Maulik")); //using .inverse
+console.log(chalk.bold("Chhabra")); //using .bold
+console.log(chalk.bold.red.inverse('Hello!')); //using .bold with RGB
+//Challenge completed
