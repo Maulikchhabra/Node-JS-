9e69434f583d424655b5d1912fd85d541c728a92
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-02-12 22:35:54 +0530

    Create YArgsPackage.js

---
+++ TopicsAndStuff/YArgsPackage.js
@@ -0,0 +1,6 @@
+const yargs =require('yargs')
+//console.log(process.argv);
+console.log(yargs.argv);
+//By default if we do node app --version it will show 1.0.0
+//To customize the version of yargs
+yargs.version('1.1.0') //setting version to 1.1.0 from 1.0.0
