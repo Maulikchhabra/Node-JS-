439218d96b0030350616465214bddd504089ad33
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-02-18 22:15:44 +0530

    Chalk Package from NPM

---
+++ TopicsAndStuff/ChalkPackage.js
@@ -0,0 +1,6 @@
+//Chalk package
+const chalk =require('chalk') //Importing chalk package
+console.log(chalk.green("Success!")); //using .green method to turn string in green
+console.log(chalk.inverse("Maulik")); //using .inverse
+console.log(chalk.bold("Chhabra")); //using .bold
+console.log(chalk.bold.red.inverse('Hello!')); //using .bold with RGB
