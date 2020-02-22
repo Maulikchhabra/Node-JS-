f3d65af3f7aad36aa0e690a3a8cd61af2e9beed1
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-02-22 13:57:24 +0530

    Complete add function challenge

---
+++ Challenges/challenge6/app.js
@@ -0,0 +1,27 @@
+//Challenge 5
+ //Complete add command
+/*
+yargs.command({
+   command:'add',
+   descbribe:'Add new note',
+   builder:{
+     title:{
+       describe:'Note list',
+       demandOption: true,
+       type:'string'
+     },
+     body:{
+       describe:'Body of note',
+       demandOption:true,
+       type:'string'
+     }
+   },
+   handler:function(argv){
+     console.log("Adding to the notes", argv);
+     }
+ })
+ console.log("Title:"+ argv.title);
+ console.log("Body:"+ argv.body);
+ */
+//Challenge completed
+
