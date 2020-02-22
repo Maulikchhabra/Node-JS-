2e8ef986aa17d03a1e79240757e24dcd43abd1d2
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-02-22 13:53:52 +0530

    read command added

--- Notes App/app.js
+++ Notes App/app.js
@@ -56,3 +56,19 @@ const notes =require('./notes.js')
    }
  })   
  //console.log(yargs.argv);
+
+//create read command
+ yargs.command({
+   command:'read',
+   describe:'Read the notes',
+   builder:{
+     title:{
+            descbribe:'Note title',
+            demandOption:true,
+            type:'string' 
+     }
+   },
+   handler(argv){notes.readNote(argv.title)
+   }
+ })
+ console.log(yargs.argv);
