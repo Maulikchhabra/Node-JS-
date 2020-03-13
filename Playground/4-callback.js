273e7eee35968785edd2b520e1f03cb91d63ae25
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-03-13 22:26:08 +0530

    Update 4-callback.js

--- Playground/4-callback.js
+++ Playground/4-callback.js
@@ -7,3 +7,16 @@ const names =['Maulik','john','smith','alex','light','kira']
 const shortNames =names.filter((name)=>{
     return name.length <= 4
 })
+
+/*
+const geoCode =(address, callback) =>{
+   const data ={
+       latitude:0,
+       longitude:0
+   }
+   return data
+}
+
+const data=geoCode('India')
+console.log(data);
+*/
