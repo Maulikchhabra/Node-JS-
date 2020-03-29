916d45839fe980bec3976d0814dc3a5f62ac5d5d
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-03-29 10:59:03 +0530

    Included output

--- TopicsAndStuff/setTimeout.js
+++ TopicsAndStuff/setTimeout.js
@@ -9,3 +9,18 @@ setTimeout(()=>{console.log("0");   //Output 2
 },0)
 
 console.log("Ending");
+
+/*
+Outputs=
+
+Output1 =E:\html_css_js\New folder (2)\nodejs\weatherApp>node app.js
+Starting
+Ending 
+3
+
+Output2 =E:\html_css_js\New folder (2)\nodejs\weatherApp>node app.js
+Starting
+Ending
+0
+3
+*/
