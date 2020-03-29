1ffaab03fe8f385a7b7d744396fa77f6cdda8750
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-03-29 10:57:10 +0530

    Create setTimeout.js

---
+++ TopicsAndStuff/setTimeout.js
@@ -0,0 +1,11 @@
+/*console.log("Starting");
+
+setTimeout(()=>{ 
+    console.log("3");    //Output 1
+    
+},3000)
+
+setTimeout(()=>{console.log("0");   //Output 2
+},0)
+
+console.log("Ending");
