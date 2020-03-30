ba4ab59504e1983068ab892c2eb2b04dd01e7132
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-03-30 20:13:36 +0530

    Update app.js

--- Weather App/Simple WebApp/app.js
+++ Weather App/Simple WebApp/app.js
@@ -2,3 +2,29 @@ const chalk = require('chalk')
 const request =require('request')
 const geoCode = require('./utils/geoCode')
 const forecast =require('./utils/forecast')
+
+
+const address =process.argv[2]
+
+if(!address){
+  console.log('Please provide an address!!');
+}
+else{
+  geoCode(address,(error, {lat, long, location})=>{
+    if(error){
+      return console.log(error);
+    }
+   // console.log('Error:',error);
+  //console.log('Data:',data);
+    forecast(lat, long, (error, forecastData) => {
+      if(error){
+        return console.log(error);
+      }
+  
+      //console.log('Error:', error)
+      console.log('Data:', location);
+      console.log(forecastData);
+      
+    })
+  })
+}
