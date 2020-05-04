b972f8000e8ccd35d1a908b4152fbf1967145291
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-05-04 23:49:49 +0530

    Update forecast.js

--- Weather App/Simple WebApp/utils/forecast.js
+++ Weather App/Simple WebApp/utils/forecast.js
@@ -18,3 +18,22 @@ const forecast =(lat, long, callback)=>{
       })
 }
 */
+
+const forecast =(lat, long, callback)=>{
+    const url ='https://api.darksky.net/forecast/1cfa8494916eb140e6d9266d0a6082d6/'+lat+','+long
+
+    request({url, json:true },(error, { body })=>{
+        //console.log(error);
+        if(error){
+          callback('unable to connect to weather service!', undefined);
+        }
+        else if(body.error){
+          callback("Unable to find location!", undefined); 
+        }
+        else{
+          callback(undefined, body.daily.data[0].summary + "It is currently "+body.currently.temperature +" degrees out. There is a "+body.currently.precipProbability+" % chance of rain.");
+        }   
+      })
+}
+
+module.exports= forecast
