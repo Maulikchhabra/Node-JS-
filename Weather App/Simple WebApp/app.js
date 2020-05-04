e3b454926710c36a7df7dfbfffad29e5038f52e7
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-05-04 23:45:40 +0530

    Update app.js

--- Weather App/Simple WebApp/app.js
+++ Weather App/Simple WebApp/app.js
@@ -27,4 +27,33 @@ else{
       
     })
   })
+  
+  /*
+Before object destructuring
+
+if(!address){
+  console.log('Please provide an address!!');
+}
+else{
+  geoCode(address,(error, data)=>{
+    if(error){
+      return console.log(error);
+    }
+   // console.log('Error:',error);
+  //console.log('Data:',data);
+    forecast(data.lat, data.long, (error, forecastData) => {
+      if(error){
+        return console.log(error);
+      }
+  
+      //console.log('Error:', error)
+      console.log('Data:', data.location);
+      console.log(forecastData);
+      
+    })
+  })
+}
+ */
+//Challenge in forecast.js
+//Challenge completed
 }
